a9a2f087b49d373c8f37d5e1f8227a0138952b7b
Derek Williams <derek@nebvin.ca>
2010-05-20 10:29:38 -0600

    Don't package logback

--- project/build/WilliamsFamilyProject.scala
+++ project/build/WilliamsFamilyProject.scala
@@ -21,8 +21,8 @@ class WilliamsFamilyProject(info: ProjectInfo) extends DefaultWebProject(info)
   val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.11"
   val jcloverslf4j = "org.slf4j" % "jcl-over-slf4j" % "1.5.11"
   val log4joverslf4j = "org.slf4j" % "log4j-over-slf4j" % "1.5.11"
-  val logback = "ch.qos.logback" % "logback-classic" % "0.9.18"
-  
+
+  val logback = "ch.qos.logback" % "logback-classic" % "0.9.18" % "test->default"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.4" % "test->default"
   val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.7-SNAPSHOT" % "test->default"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.23" % "test->default"
